d242630890e47d7214028a850ea64cd8262b50af
Weder Videira Borges <wederborgesemp@gmail.com>
2025-11-10 22:45:41 -0400

    Estruturando blocos no HTML e organização da rota de perfil

--- pinterest/routes.py
+++ pinterest/routes.py
@@ -1,7 +1,11 @@
 from pinterest import app
-from flask import render_template
+from flask import render_template, url_for
 
 
 @app.route("/")
 def home():
     return render_template('homepage.html')
+
+@app.route("/perfil/<id_usuario>")
+def perfil(id_usuario):
+    return render_template('perfil.html', id_usuario=id_usuario)
